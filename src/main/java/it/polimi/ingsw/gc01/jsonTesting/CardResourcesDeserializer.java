530b150206bdaf3b1db7fa5f5b3f8a952ad27175
M1RK02 <picamirko02@gmail.com>
2024-04-16 00:23:24 +0200

    Replaced shit with chocolate Thank me later!

--- src/main/java/it/polimi/ingsw/gc01/jsonTesting/CardResourcesDeserializer.java
+++ src/main/java/it/polimi/ingsw/gc01/jsonTesting/CardResourcesDeserializer.java
@@ -2,20 +2,15 @@ package it.polimi.ingsw.gc01.jsonTesting;
 
 import com.google.gson.*;
 import java.lang.reflect.Type;
+import java.util.stream.*;
 import it.polimi.ingsw.gc01.model.*;
 import it.polimi.ingsw.gc01.model.corners.*;
 
 public class CardResourcesDeserializer implements JsonDeserializer<CardResource> {
     public CardResource deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
-        try {
-            return CornerValue.valueOf(json.getAsString());
-
-        } catch (IllegalArgumentException ignored) {}
-
-        try {
-            return Item.valueOf(json.getAsString());
-        } catch (IllegalArgumentException ignored) {}
-
-        return Resource.valueOf(json.getAsString());
+        return Stream.of(CornerValue.values(), Item.values(), Resource.values())
+                .flatMap(Stream::of)
+                .filter(x -> x.name().equals(json.getAsString()))
+                .findAny().orElse(null);
     }
 }
