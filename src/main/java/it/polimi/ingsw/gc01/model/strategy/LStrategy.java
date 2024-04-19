7ed874ebbc94084e1c4b83ad80e3b139f69c4c52
M1RK02 <picamirko02@gmail.com>
2024-04-19 11:28:23 +0200

    Implemented LStrategy

--- src/main/java/it/polimi/ingsw/gc01/model/strategy/LStrategy.java
+++ src/main/java/it/polimi/ingsw/gc01/model/strategy/LStrategy.java
@@ -1,10 +1,11 @@
 package it.polimi.ingsw.gc01.model.strategy;
 
+import java.util.*;
 import it.polimi.ingsw.gc01.model.cards.*;
 import it.polimi.ingsw.gc01.model.player.*;
 
 public class LStrategy implements Strategy {
-    private CardColor bodyColor;
+    private final CardColor bodyColor;
 
     public LStrategy(CardColor bodyColor) {
         this.bodyColor = bodyColor;
@@ -15,7 +16,63 @@ public class LStrategy implements Strategy {
     }
 
     public int check(Player player){
-        // TODO
-        return 0;
+        Position origin = new Position(0,0);
+        Map<Position, PlayableCard> field = player.getField().getPositions();
+        CardColor leafColor = getLeafColor(bodyColor);
+        Set<Position> found = new HashSet<>();
+        for(Position p : field.keySet()){
+            if (!found.contains(p) && !p.equals(origin)) {
+                ResourceCard c = (ResourceCard) field.get(p);
+                if (c.getColor().equals(leafColor)) {
+                    Set <Position> body = getBodyIfRight(field, p);
+                    if (body != null) {
+                        found.add(p);
+                        found.addAll(body);
+                    }
+                }
+            }
+        }
+        return found.size();
+    }
+
+    private CardColor getLeafColor(CardColor bodyColor) {
+        switch (bodyColor) {
+            case RED:
+                return CardColor.GREEN;
+            case GREEN:
+                return CardColor.PURPLE;
+            case BLUE:
+                return CardColor.RED;
+        }
+        // case PURPLE
+        return CardColor.BLUE;
+    }
+
+    private Set<Position> getBodyIfRight(Map<Position, PlayableCard> field, Position p){
+        Position upperBody = null, lowerBody = null;
+        if (bodyColor.equals(CardColor.RED)) {
+            upperBody = new Position(p.getX() - 1, p.getY() + 2);
+            lowerBody = new Position(p.getX() - 1, p.getY() + 1);
+        }
+        if (bodyColor.equals(CardColor.GREEN)) {
+            upperBody = new Position(p.getX() + 1, p.getY() + 2);
+            lowerBody = new Position(p.getX() + 1, p.getY() + 1);
+        }
+        if (bodyColor.equals(CardColor.BLUE)) {
+            upperBody = new Position(p.getX() - 1, p.getY() - 1);
+            lowerBody = new Position(p.getX() - 1, p.getY() - 2);
+        }
+        if (bodyColor.equals(CardColor.PURPLE)) {
+            upperBody = new Position(p.getX() + 1, p.getY() - 1);
+            lowerBody = new Position(p.getX() + 1, p.getY() - 2);
+        }
+        try {
+            ResourceCard upperCard = (ResourceCard) field.get(upperBody);
+            ResourceCard lowerCard = (ResourceCard) field.get(lowerBody);
+            if (upperCard.getColor().equals(bodyColor) && lowerCard.getColor().equals(bodyColor)) {
+                return Set.of(upperBody, lowerBody);
+            }
+        } catch (ClassCastException ignore){}
+        return null;
     }
 }
