7ac6ce312da961ec9ecf601e066ffb23542ea729
M1RK02 <picamirko02@gmail.com>
2024-05-20 20:18:32 +0200

    Removed Socket and Messages

--- src/main/java/it/polimi/ingsw/gc01/network/message/JoinFirstGameMessage.java
+++
@@ -1,7 +0,0 @@
-package it.polimi.ingsw.gc01.network.message;
-
-public class JoinFirstGameMessage extends ClientToServerMessage {
-    public JoinFirstGameMessage(String playerName) {
-        super(playerName);
-    }
-}
--- src/main/java/it/polimi/ingsw/gc01/network/message/SwitchReadyMessage.java
+++
@@ -1,7 +0,0 @@
-package it.polimi.ingsw.gc01.network.message;
-
-public class SwitchReadyMessage extends ClientToServerMessage{
-    public SwitchReadyMessage(String playerName) {
-        super(playerName);
-    }
-}
