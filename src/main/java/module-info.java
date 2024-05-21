7085eb64196ce93a1fa949f2b04b43735ff069d4
M1RK02 <picamirko02@gmail.com>
2024-05-21 15:36:45 +0200

    Fixed module info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,8 +4,7 @@ module it.polimi.ingsw.gc01 {
     requires com.google.gson;
     requires java.rmi;
 
-    exports it.polimi.ingsw.gc01;
-    exports it.polimi.ingsw.gc01.model;
+    opens it.polimi.ingsw.gc01.model to com.google.gson;
     opens it.polimi.ingsw.gc01.model.cards to com.google.gson;
     opens it.polimi.ingsw.gc01.model.corners to com.google.gson;
     opens it.polimi.ingsw.gc01.network to java.rmi;
