5728d43feb20aa7ed4a934b12abdb43da8655c7d
M1RK02 <picamirko02@gmail.com>
2024-05-21 16:20:22 +0200

    Moved main into main client

--- src/main/java/it/polimi/ingsw/gc01/UI/TUI.java
+++ src/main/java/it/polimi/ingsw/gc01/UI/TUI.java
@@ -8,19 +8,20 @@ import java.rmi.RemoteException;
 import java.util.Scanner;
 
 public class TUI extends UI {
-
     private NetworkClient client;
 
     public TUI(){
     }
+
     public void printTitle(){
-        System.out.println("\t\t\t\t\t\t\t\t\t ██████╗ ██████╗ ██████╗ ███████╗██╗  ██╗\n" +
+        System.out.println(
+                "\t\t\t\t\t\t\t\t\t ██████╗ ██████╗ ██████╗ ███████╗██╗  ██╗\n" +
                 "\t\t\t\t\t\t\t\t\t██╔════╝██╔═══██╗██╔══██╗██╔════╝╚██╗██╔╝\n" +
                 "\t\t\t\t\t\t\t\t\t██║     ██║   ██║██║  ██║█████╗   ╚███╔╝ \n" +
                 "\t\t\t\t\t\t\t\t\t██║     ██║   ██║██║  ██║██╔══╝   ██╔██╗ \n" +
                 "\t\t\t\t\t\t\t\t\t╚██████╗╚██████╔╝██████╔╝███████╗██╔╝ ██╗\n" +
-                " \t\t\t\t\t\t\t\t\t╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝\n" +
-                "                                         \n" +
+                "\t\t\t\t\t\t\t\t\t╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝\n" +
+                "\n" +
                 "\t\t\t\t\t\t███╗   ██╗ █████╗ ████████╗██╗   ██╗██████╗  █████╗ ██╗     ██╗███████╗\n" +
                 "\t\t\t\t\t\t████╗  ██║██╔══██╗╚══██╔══╝██║   ██║██╔══██╗██╔══██╗██║     ██║██╔════╝\n" +
                 "\t\t\t\t\t\t██╔██╗ ██║███████║   ██║   ██║   ██║██████╔╝███████║██║     ██║███████╗\n" +
@@ -30,36 +31,39 @@ public class TUI extends UI {
 
         );
     }
+
     public String askPlayerName(){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Type in the nickname that you want to use to play CODEX NATURALIS (lazzar edidtion)");
         return scanner.nextLine();
-
-
-
     }
+
     public void askAndSetIP(){
         System.out.println("Type in the Server IP or leave emtpy for localhost:\n");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-
         if (input.isEmpty()){
             DefaultValue.ServerIp = "localhost";
         } else {
             DefaultValue.ServerIp = input;
         }
-
-
-
     }
+
     public void askAndSetPort(){
         System.out.println("On which port are you going to connect your computer?\n" );
         Scanner scanner = new Scanner(System.in);
-        DefaultValue.RMIPort = scanner.nextInt();
+        String input = scanner.nextLine();
+        if (input.isEmpty()){
+            DefaultValue.RMIPort = 1234;
+        } else {
+            DefaultValue.RMIPort = Integer.parseInt(input);
+        }
     }
+
     public void createRMIClient(String playerName) throws RemoteException {
         this.client = new RmiClient(playerName, this);;
     }
+
     public void askModalityToEnterGame() throws RemoteException {
         System.out.println("Would you rather:\n" +
                 "(1) Create a new Game\n" +
@@ -72,44 +76,22 @@ public class TUI extends UI {
                 this.client.createGame();
                 System.out.println("New game created, waiting for more players...\n");
                 break;
-
             case (2):
                 this.client.joinFirstGame();
                 System.out.println("Joining the first Available Game\n");
                 break;
-
-
-
-
             case (3):
                 System.out.println("Type the roomId of the Game you want to join...\n");
                 String roomId = scanner.nextLine();
                 this.client.joinGame(roomId);
                 System.out.println("Joining the Game...\n");
                 break;
-
         }
-
     }
 
     @Override
     public void showRoom(String roomId) {
-        System.out.println("Sto mostrando la room");
-    }
-
-
-
-    public static void main(String[] args) throws RemoteException {
-
-        TUI tui = new TUI();
-        tui.printTitle();
-        String playerName = tui.askPlayerName();
-        tui.askAndSetIP();
-        tui.askAndSetPort();
-        tui.createRMIClient(playerName);
-        tui.askModalityToEnterGame();
-
-
+        System.out.println("Sto mostrando la room che ho joinato");
+        System.out.println(roomId);
     }
 }
-
--- src/main/java/it/polimi/ingsw/gc01/main/MainClient.java
+++ src/main/java/it/polimi/ingsw/gc01/main/MainClient.java
@@ -1,42 +1,17 @@
 package it.polimi.ingsw.gc01.main;
 
+import it.polimi.ingsw.gc01.UI.TUI;
+
+import java.rmi.RemoteException;
+
 public class MainClient {
-
-    public void printTitle(){
-        System.out.println("\t\t\t\t\t\t\t\t\t ██████╗ ██████╗ ██████╗ ███████╗██╗  ██╗\n" +
-                          "\t\t\t\t\t\t\t\t\t██╔════╝██╔═══██╗██╔══██╗██╔════╝╚██╗██╔╝\n" +
-                          "\t\t\t\t\t\t\t\t\t██║     ██║   ██║██║  ██║█████╗   ╚███╔╝ \n" +
-                          "\t\t\t\t\t\t\t\t\t██║     ██║   ██║██║  ██║██╔══╝   ██╔██╗ \n" +
-                          "\t\t\t\t\t\t\t\t\t╚██████╗╚██████╔╝██████╔╝███████╗██╔╝ ██╗\n" +
-                          " \t\t\t\t\t\t\t\t\t╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝\n" +
-                "                                         \n" +
-                "\t\t\t\t\t\t███╗   ██╗ █████╗ ████████╗██╗   ██╗██████╗  █████╗ ██╗     ██╗███████╗\n" +
-                "\t\t\t\t\t\t████╗  ██║██╔══██╗╚══██╔══╝██║   ██║██╔══██╗██╔══██╗██║     ██║██╔════╝\n" +
-                "\t\t\t\t\t\t██╔██╗ ██║███████║   ██║   ██║   ██║██████╔╝███████║██║     ██║███████╗\n" +
-                "\t\t\t\t\t\t██║╚██╗██║██╔══██║   ██║   ██║   ██║██╔══██╗██╔══██║██║     ██║╚════██║\n" +
-                "\t\t\t\t\t\t██║ ╚████║██║  ██║   ██║   ╚██████╔╝██║  ██║██║  ██║███████╗██║███████║\n" +
-                "\t\t\t\t\t\t╚═╝  ╚═══╝╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝╚══════╝\n"
-
-                );
+    public static void main(String[] args) throws RemoteException {
+        TUI tui = new TUI();
+        tui.printTitle();
+        String playerName = tui.askPlayerName();
+        tui.askAndSetIP();
+        tui.askAndSetPort();
+        tui.createRMIClient(playerName);
+        tui.askModalityToEnterGame();
     }
-
- public void askIP(){
-        System.out.println("Write Remote IP to connect the server");
-
- }
-
-
-
-
-    public static void main(String[] args) {
-    MainClient client = new MainClient();
-    //RmiClient rmiClient = new RmiClient();
-    client.printTitle();
-
-
-
-
-
-    }
-    }
-
+}
